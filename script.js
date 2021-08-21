58da16cc9c4e59d409ed2f02e6be9ca6c98eeebb
mckalechung <71574223+mckalechung@users.noreply.github.com>
2021-08-20 18:01:46 -0600

    Add files via upload
    Adding changes

---
+++ script.js
@@ -0,0 +1,15 @@
+function showDropDown() {
+ 
+    if(document.getElementById('fafa').classList.contains('fa-times')){
+    document.getElementById('navbar-links').style.display = 'none'
+      document.getElementById('fafa').classList.remove('fa-times');
+      document.getElementById('fafa').classList.add('fa-bars');
+    }
+  
+    else if(document.getElementById('fafa').classList.contains('fa-bars')){
+      document.getElementById('navbar-links').style.display = 'block'
+    document.getElementById('fafa').classList.remove('fa-bars');
+    document.getElementById('fafa').classList.add('fa-times');
+  }
+  }
+  
