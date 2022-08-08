86baf98d828baa229354f9e46ff3a063dfefb95a
McKale Chung <mckale.chung@gmail.com>
2022-08-07 22:01:01 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import Works from './components/Works/Works'
 import Illustration from './components/Illustration/Illustration'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Menu from './components/Menu/Menu'
 import './app.scss'
+import { useState } from 'react';
 
 const App = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='app'>
         
-            <Navbar />
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
             <div className="sections">
                 <Intro />
                 <Portfolio />
