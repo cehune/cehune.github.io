9507d7f58fbc1afb3481fead703de3e70cebde31
Mckale Chung <mckale.chung@gmail.com>
2022-12-22 22:44:51 -0500

    StARTING WITH ROUTER

---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className='projects' id = 'projects'> 
+            Projects lmaooooo t girl is coding on christmas break :/
+        </div>
+    )
+}
+
+export default Projects;
